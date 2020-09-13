afc5e72e21a3ca7cf440110b754eff9c7ab5ee90
MalwareMix <57842329+MalwareMix@users.noreply.github.com>
2020-09-13 11:48:49 -0400

    nevergannagiveyouup
    nevergannagiveyouup

---
+++ nevergannagiveyouup.ino
@@ -0,0 +1,25 @@
+#include <Keyboard.h>
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Keyboard.begin();
+  delay(1000);
+ 
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  delay(10);
+  Keyboard.releaseAll();
+  delay(200);
+ 
+  Keyboard.print("chrome https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  Keyboard.press(KEY_RETURN);
+  delay(10);
+  Keyboard.releaseAll();
+  Keyboard.end();
+ 
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+}
